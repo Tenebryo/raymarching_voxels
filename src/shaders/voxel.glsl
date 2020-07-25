0a01e1efdf265fca9ea8ea1085d45b89dbc64699
Sam Blazes <blazes.sam@gmail.com>
2020-07-25 03:48:01 -0700

    improve raytracing performance (by removing conditionals and using GPU instrinsics)

--- src/shaders/voxel.glsl
+++ src/shaders/voxel.glsl
@@ -11,6 +11,10 @@
 #define MAX_DAG_DEPTH (16)
 #endif
 
+#ifndef LOD_CUTOFF_CONSTANT
+#define LOD_CUTOFF_CONSTANT (0.002)
+#endif
+
 #define MIN_VOXEL_SIZE (1.0 / pow(2, MAX_DAG_DEPTH))
 
 #define MAX_DIST 1e9
@@ -43,41 +47,32 @@ vec2 project_cube(vec3 id, vec3 od, vec3 mn, vec3 mx, out uint incidence_min, ou
     vec3 tmx = fma(id, mx, od);
 
     float ts;
-    if (tmn.x > tmn.y) {
-        if (tmn.x > tmn.z) {
-            incidence_min = INCIDENCE_X;
-            ts = tmn.x;
-        } else {
-            incidence_min = INCIDENCE_Z;
-            ts = tmn.z;
-        }
+    if (tmn.x > tmn.z) {
+        incidence_min = INCIDENCE_X;
+        ts = tmn.x;
     } else {
-        if (tmn.y > tmn.z) {
-            incidence_min = INCIDENCE_Y;
-            ts = tmn.y;
-        } else {
-            incidence_min = INCIDENCE_Z;
-            ts = tmn.z;
-        }
+        incidence_min = INCIDENCE_Z;
+        ts = tmn.z;
+    }
+
+    if (tmn.y > ts) {
+        incidence_min = INCIDENCE_Y;
+        ts = tmn.y;
     }
 
     float te;
-    if (tmx.x < tmx.y) {
-        if (tmx.x < tmx.z) {
-            incidence_max = INCIDENCE_X;
-            te = tmx.x;
-        } else {
-            incidence_max = INCIDENCE_Z;
-            te = tmx.z;
-        }
+    
+    if (tmx.x < tmx.z) {
+        incidence_max = INCIDENCE_X;
+        te = tmx.x;
     } else {
-        if (tmx.y < tmx.z) {
-            incidence_max = INCIDENCE_Y;
-            te = tmx.y;
-        } else {
-            incidence_max = INCIDENCE_Z;
-            te = tmx.z;
-        }
+        incidence_max = INCIDENCE_Z;
+        te = tmx.z;
+    }
+
+    if (tmx.y < te) {
+        incidence_max = INCIDENCE_Y;
+        te = tmx.y;
     }
 
     return vec2(ts, te);
@@ -144,31 +139,48 @@ bool voxel_valid_bit(uint parent, uint idx) {
     return voxels[parent].sub_voxels[idx] != 0;
 }
 
+#define SUBVOXEL_VALID(sv) (sv != 0)
+
 bool voxel_leaf_bit(uint parent, uint idx) {
     return voxels[parent].sub_voxels[idx] < 0;
 }
+
+#define SUBVOXEL_LEAF(sv) (sv < 0)
 
 bool voxel_empty(uint parent, uint idx) {
     return voxels[parent].sub_voxels[idx] == 0;
 }
 
+#define SUBVOXEL_EMPTY(sv) (sv != 0)
+
 uint voxel_get_child(uint parent, uint idx) {
     return voxels[parent].sub_voxels[idx] - 1;
+}
+
+#define SUBVOXEL_CHILD(sv) (sv - 1)
+
+int voxel_get_subvoxel(uint parent, uint idx) {
+    return voxels[parent].sub_voxels[idx];
 }
 
 uint voxel_get_material(uint parent, uint idx) {
     return -voxels[parent].sub_voxels[idx];
 }
 
+#define SUBVOXEL_MATERIAL(sv) (-sv)
+
 bool interval_nonempty(vec2 t) {
     return t.x < t.y;
 }
 vec2 interval_intersect(vec2 a, vec2 b) {
-    return ((b.x > a.y || a.x > b.y) ? vec2(1,0) : vec2(max(a.x,b.x), min(a.y, b.y)));
+    return vec2(max(a.x,b.x), min(a.y, b.y));
+    // return ((b.x > a.y || a.x > b.y) ? vec2(1,0) : vec2(max(a.x,b.x), min(a.y, b.y)));
 }
 
 uint select_child(vec3 pos, float scale, vec3 o, vec3 d, float t) {
-    vec3 p = o + d * t - pos - scale;
+    vec3 p = fma(d, vec3(t), o) - pos - scale;
+    // vec3 p = o + d * t - pos - scale;
+
     uint idx = 0;
 
     idx |= p.x < 0 ? 0 : AXIS_X_MASK;
@@ -178,62 +190,49 @@ uint select_child(vec3 pos, float scale, vec3 o, vec3 d, float t) {
     return idx;
 }
 
-uvec3 child_cube( uvec3 pos, uint scale, uint idx) {
-    const uvec3 child_pos_offsets[8] = {
-        uvec3(0,0,0),
-        uvec3(1,0,0),
-        uvec3(0,1,0),
-        uvec3(1,1,0),
-        uvec3(0,0,1),
-        uvec3(1,0,1),
-        uvec3(0,1,1),
-        uvec3(1,1,1)
-    };
-
-    return pos + (scale * child_pos_offsets[idx]);
+uint select_child_bit(vec3 pos, float scale, vec3 o, vec3 d, float t) {
+    vec3 p = fma(d, vec3(t), o) - pos - scale;
+    
+    uvec3 s = uvec3(uint(p.x > 0), uint(p.y > 0), uint(p.z > 0));
+    
+    return AXIS_X_MASK * s.x + AXIS_Y_MASK * s.y + AXIS_Z_MASK * s.z;
 }
 
-uint ilog2(uint v) {
-#define LT(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
-    const uint LogTable256[256] = {
-        -1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
-        LT(4), LT(5), LT(5), LT(6), LT(6), LT(6), LT(6),
-        LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7)
-    };
+uvec3 child_cube( uvec3 pos, uint scale, uint idx) {
 
-    uint r;     // r will be lg(v)
-    uint t, tt; // temporaries
-    tt = v >> 16;
-    if (tt != 0)
-    {
-        t = tt >> 8;
-        r = t != 0 ? 24 + LogTable256[t] : 16 + LogTable256[tt];
-    }
-    else 
-    {
-        t = v >> 8;
-        r = t != 0 ? 8 + LogTable256[t] : LogTable256[v];
-    }
+    uvec3 offset = uvec3(
+        bitfieldExtract(idx, 0, 1),
+        bitfieldExtract(idx, 1, 1),
+        bitfieldExtract(idx, 2, 1)
+    );
 
-    return r;
+    return pos + (scale * offset);
 }
 
 uint highest_differing_bit(uvec3 a, uvec3 b) {
     uvec3 t = a ^ b;
 
-    return ilog2(t.x | t.y | t.z);
+    return findMSB(t.x | t.y | t.z);
 }
 
 uint extract_child_slot(uvec3 pos, uint scale) {
-    uvec3 d = pos & scale;
+
+    uvec3 d = (pos & scale);
 
     uint idx = 0;
-    
+
     idx |= (d.x == 0) ? 0 : AXIS_X_MASK;
     idx |= (d.y == 0) ? 0 : AXIS_Y_MASK;
     idx |= (d.z == 0) ? 0 : AXIS_Z_MASK;
 
     return idx;
+}
+
+uint extract_child_slot_bfe(uvec3 pos, uint depth) {
+
+    uvec3 d = bitfieldExtract(pos, int(depth), 1);
+
+    return AXIS_X_MASK * d.x + AXIS_Y_MASK * d.y + AXIS_Z_MASK * d.z;
 }
 
 #define VOXEL_MARCH_MISS 0
@@ -248,17 +247,6 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
 
     const uint MAX_SCALE = (1<<MAX_DAG_DEPTH);
 
-    const ivec3 incidence_axis[3] = {
-        ivec3(1,0,0),
-        ivec3(0,1,0),
-        ivec3(0,0,1)
-    };
-    const uint incidence_mask[3] = {
-        AXIS_X_MASK,
-        AXIS_Y_MASK,
-        AXIS_Z_MASK
-    };
-
     uint pstack[MAX_DAG_DEPTH];
     float tstack[MAX_DAG_DEPTH];
     uint dmask = 0;
@@ -266,9 +254,11 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
     vec3 ds = sign(d);
 
     d *= ds;
-    o = o * ds + (1 - ds) * 0.5;
+    // o = o * ds + (1 - ds) * 0.5;
+    o = fma(o, ds, (1 - ds) * 0.5);
 
     o *= MAX_SCALE;
+    d *= MAX_SCALE;
 
     dmask |= ds.x < 0 ? AXIS_X_MASK : 0;
     dmask |= ds.y < 0 ? AXIS_Y_MASK : 0;
@@ -280,7 +270,6 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
     vec3 od = - o * id;
 
     max_dist *= MAX_SCALE;
-
 
     vec2 t = vec2(0, max_dist);
 
@@ -311,42 +300,37 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
         return false;
     }
 
-    
-
-
     scale = scale >> 1;
-    idx = select_child(pos, scale, o, d, t.x);
+    // idx = select_child(pos, scale, o, d, t.x);
+    idx = select_child_bit(pos, scale, o, d, t.x);
     pos = child_cube(pos, scale, idx);
-
-    float test_idx = idx ^ dmask;
-
 
     return_state = VOXEL_MARCH_MISS;
 
     pstack[0] = parent;
     tstack[0] = t.y;
-
+    vec2 tc, tv;
 
     // very hot loop
     while (iterations < 512) {
         iterations += 1;
 
-
         uint new_incidence;
 
-        vec2 tc = project_cube(id, od, pos, pos + scale, incidence_min, new_incidence);
+        tc = project_cube(id, od, pos, pos + scale, incidence_min, new_incidence);
 
-        if (voxel_valid_bit(parent, dmask ^ idx) && interval_nonempty(t)) {
+        int subvoxel = voxel_get_subvoxel(parent, dmask ^ idx);
 
-            if (scale <= tc.x * 0.002 || depth >= max_depth) {
+        if (SUBVOXEL_VALID(subvoxel) && interval_nonempty(t)) {
+
+            if (scale <= tc.x * LOD_CUTOFF_CONSTANT || depth >= max_depth) {
 
                 // voxel is too small
-                dist = t.x / MAX_SCALE;
+                dist = t.x;
                 return_state = depth >= max_depth ? VOXEL_MARCH_MAX_DEPTH : VOXEL_MARCH_LOD;
                 material = lod_materials[parent];
                 return true;
             }
-        
 
             if (tc.x > max_dist) {
                 // voxel is beyond the render distance
@@ -354,14 +338,14 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
                 return false;
             }
 
-            vec2 tv = interval_intersect(tc, t);
+            tv = interval_intersect(tc, t);
 
             if (interval_nonempty(tv)) {
-                if (voxel_leaf_bit(parent, dmask ^ idx)) {
-                    dist = tv.x / MAX_SCALE;
+                if (SUBVOXEL_LEAF(subvoxel)) {
+                    dist = tv.x;
                     vid = (parent << 3) | (dmask ^ idx);
                     return_state = VOXEL_MARCH_HIT;
-                    material = voxel_get_material(parent, dmask ^ idx);
+                    material = SUBVOXEL_MATERIAL(subvoxel);
                     return true;
                 }
                 // descend:
@@ -373,15 +357,15 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
 
                 h = tc.y;
                 scale = scale >> 1;
-                parent = voxel_get_child(parent, dmask ^ idx);
-                idx = select_child(pos, scale, o, d, tv.x);
+                parent = SUBVOXEL_CHILD(subvoxel);
+                // idx = select_child(pos, scale, o, d, tv.x);
+                idx = select_child_bit(pos, scale, o, d, tv.x);
                 t = tv;
                 pos = child_cube(pos, scale, idx);
 
                 continue;
             }
         }
-
 
         incidence = new_incidence;
 
@@ -390,40 +374,46 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
 
         uint mask = 0;
         uint bit_diff = 0;
-        if (incidence == INCIDENCE_X) {
+
+        switch (incidence) {
+        case INCIDENCE_X:
             uint px = pos.x;
             pos.x += scale;
             bit_diff = px ^ pos.x;
-            mask = AXIS_X_MASK;
-        } else if (incidence == INCIDENCE_Y) {
+            // mask = AXIS_X_MASK;
+            break;
+        case INCIDENCE_Y:
             uint py = pos.y;
             pos.y += scale;
             bit_diff = py ^ pos.y;
-            mask = AXIS_Y_MASK;
-        } else {
+            // mask = AXIS_Y_MASK;
+            break;
+        case INCIDENCE_Z:
             uint pz = pos.z;
             pos.z += scale;
             bit_diff = pz ^ pos.z;
-            mask = AXIS_Z_MASK;
+            // mask = AXIS_Z_MASK;
+            break;
         }
 
+        mask = (1 << incidence);
         idx ^= mask;
-
 
         // idx bits should only ever flip 0->1 because we force the ray direction to always be in the (1,1,1) quadrant
         if ((idx & mask) == 0) {
             // ascend
 
             // highest differing bit
-            depth = ilog2(bit_diff);
+            // depth = ilog2(bit_diff);
+            uint idepth = findMSB(bit_diff);
 
             // check if we exited voxel tree
-            if (depth >= MAX_DAG_DEPTH) {
+            if (idepth >= MAX_DAG_DEPTH) {
                 return_state = VOXEL_MARCH_MISS;
                 return false;
             }
 
-            depth = MAX_DAG_DEPTH - depth;
+            depth = MAX_DAG_DEPTH - idepth;
 
             scale = MAX_SCALE >> depth;
             // scale = 1 << (MAX_DAG_DEPTH - 1 - depth);
@@ -432,15 +422,18 @@ bool voxel_march(vec3 o, vec3 d, uint max_depth, float max_dist, out float dist,
             t.y = tstack[depth];
 
             // round position to correct voxel (mask out low bits)
-            pos &= 0xFFFFFFFF ^ (scale - 1);
+            // pos &= 0xFFFFFFFF ^ (scale - 1);
+            pos = bitfieldInsert(pos, uvec3(0), 0, int(idepth));
             
             // get the idx of the child at the new depth
-            idx = extract_child_slot(pos, scale);
+            // idx = extract_child_slot(pos, scale);
+            idx = extract_child_slot_bfe(pos, idepth);
 
             h = 0;
         }
 
     }
+
 
     return_state = VOXEL_MARCH_LOOP_END;
     return false;
